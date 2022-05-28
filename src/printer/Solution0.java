a60e895a4025ed662e82ad1215508ed6dc725012
leedongyeop <20181676@edu.hanbat.ac.kr>
2022-05-28 22:04:26 +0900

    중첩클래스를 이용한 솔루션

--- src/printer/Solution0.java
+++ src/printer/Solution0.java
@@ -1,30 +1,45 @@
 package printer;
 
-
 import java.util.*;
 
 public class Solution0 {
-    public static int solution(int[] priorities, int location) {
+    class Node {
+        int location;
+        int prior;
+
+        Node(int location, int prior) {
+            this.location = location;
+            this.prior = prior;
+        }
+    }
+    public int solution(int[] priorities, int location) {
         int answer = 0;
 
+        Queue<Node> q = new LinkedList<>();
+        for (int i = 0; i < priorities.length; i++) {
+            q.add(new Node(i, priorities[i]));
+        }
+
+        int now = 0;
+
+        dwl:
+        while (!q.isEmpty()) {
+            Node peekNode = q.peek();
+
+            for (Node n : q) {
+                if (n.prior > peekNode.prior) {
+                    q.poll();
+                    q.add(peekNode);
+                } else {
+                    now++;
+                    if (peekNode.location == location) {
+                        answer = now;
+                        break dwl;
+                    }
+                }
+            }
+        }
         return answer;
     }
-
-    public static void main(String[] args) {
-        Queue<Node> q = new LinkedList<Node>();
-        q.add(new Node('A',2));
-        q.add(new Node('B',1));
-        q.add(new Node('C',3));
-        q.add(new Node('D',2));
-    }
 }
 
-class Node {
-    char data;
-    int prior;
-
-    Node(char data, int prior) {
-        this.data = data;
-        this.prior = prior;
-    }
-}
