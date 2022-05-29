ea1ced20e45b9ad7e3934ecad511291b777dd0de
leedongyeop <20181676@edu.hanbat.ac.kr>
2022-05-29 12:56:28 +0900

    불필요한 코드 정리

--- src/printer/Solution1.java
+++ src/printer/Solution1.java
@@ -1,6 +1,4 @@
 package printer;
-
-
 import java.util.*;
 
 public class Solution1 {
@@ -28,26 +26,8 @@ public class Solution1 {
     }
 
     public static void main(String[] args) {
+
         int[] priorities = {2, 1, 3, 2};
-
         System.out.println(solution(priorities, 0));
-//        Queue<Node> q = new LinkedList<Node>();
-//        q.add(new Node('A',2));
-//        q.add(new Node('B',1));
-//        q.add(new Node('C',3));
-//        q.add(new Node('D',2));
-
-
-        //3
     }
 }
-//
-//class Node {
-//    char data;
-//    int prior;
-//
-//    Node(char data, int prior) {
-//        this.data = data;
-//        this.prior = prior;
-//    }
-//}
