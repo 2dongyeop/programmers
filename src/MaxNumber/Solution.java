b64b23882383b3fb8d6346723dace18f9952d446
leedongyeop <20181676@edu.hanbat.ac.kr>
2022-06-23 13:22:27 +0900

    숫자 비교 방식으로 접근한 풀이 - 실패

--- src/MaxNumber/Solution.java
+++ src/MaxNumber/Solution.java
@@ -1,24 +1,24 @@
-package MaxNumber;
+package maxNumber;
 
 import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
     int maxNumber;
-    List<Integer> list = new ArrayList<>();
-    List<Integer> temp = new ArrayList<>();
+    static List<Integer> list = new ArrayList<>();
+    static List<Integer> temp = new ArrayList<>();
 
     public String solution(int[] numbers) {
         arrayToList(temp, numbers); //temp에다 배열을 복사
 
-        do {
-            maxNumber = temp.get(0);
-            for (int i = 0; i < temp.size(); i++) {
+        while (!temp.isEmpty()) {
 
+            maxNumber = temp.get(0);
+            for (int i = 1; i < temp.size(); i++) {
                 int digit = temp.get(i) / 10;
                 addMaxNum(maxNumber, temp.get(i), digit, i);
             }
-        } while (list.size() == numbers.length);
+        }
 
         String answer = list.toString();
         return answer;
@@ -40,24 +40,29 @@ class Solution {
                 list.add(maxNumber);
             }
         } else if (digit < 10) { //2자리수
-            if (currentMax < num) {
+            if (currentMax * 10 < num) {
                 maxNumber = num;
                 temp.remove(index);
                 list.add(maxNumber);
             }
         } else if (digit < 100) { //3자리수
-            if (currentMax < num) {
+            if (currentMax * 100 < num) {
                 maxNumber = num;
                 temp.remove(index);
                 list.add(maxNumber);
             }
         } else if (digit == 1000) {
-            //maxNumber일리가 없다고 생각하여 과정 생략
+            if (currentMax * 1000 < num) {
+                maxNumber = num;
+                temp.remove(index);
+                list.add(maxNumber);
+            }
         }
     }
+
 }
 
 
 //int a가 0~9이면 a / 10 = 0; -> 1미만이면 1자리수
-//11~99면 1~9;                -> 10미만이면 2자리수
-//101~999면 10~99;            -> 100미만이면 3자리수
+//10~99면 1~9;                -> 10미만이면 2자리수
+//100~999면 10~99;            -> 100미만이면 3자리수
