b64eacd9f3b6e53b7c532a3ec13460418148d915
leedongyeop <20181676@edu.hanbat.ac.kr>
2022-07-03 21:27:43 +0900

    정답 코드 : PriorityQueue를 이용해 정렬을 하여 코드를 간소화

--- src/moreSpicy/Solution2.java
+++ src/moreSpicy/Solution2.java
@@ -3,29 +3,28 @@ package moreSpicy;
 import java.util.PriorityQueue;
 
 class Solution2 {
-    static int count = 0; //섞은 횟수
-    static PriorityQueue<Integer> heap = new PriorityQueue<>();
+    private int count = 0; //섞은 횟수
+    private final PriorityQueue<Integer> heap = new PriorityQueue<>();
 
     public int solution(int[] scoville, int K) {
-        arrayToQueue(heap, scoville); //잦은 삭제와 추가로 인해 리스트를 이용
+        arrayToQueue(heap, scoville);       //잦은 삭제와 추가로 인해 리스트를 이용
 
-        while (heap.peek() < K) {
-            if (heap.size() < 2) return -1; //K 이상으로 만들 수 없는 경우
-        //while (!isBiggerK(heap, K)) { //K보다 작은 값이 없을때까지 반복
-            makeSpicy(heap);          //맵게 만들기
+        while (heap.peek() < K) {           //제일 작은 수가 K보다 작으면 makeSpicy() 실행
+
+            if (heap.size() < 2) return -1; //두 수를 합해 K 이상으로 만들 수 없는 경우
+            makeSpicy(heap);                //가장 작은 두 수를 합해 맵게 만들기
         }
 
-        int answer = count;
-        return answer;
+        return count;
     }
 
-    public static void arrayToQueue(final PriorityQueue queue, final int[] array) { //배열을 리스트로 복사
+    private final void arrayToQueue(final PriorityQueue queue, final int[] array) {
         for (int a : array) {
             queue.add(a);
         }
     }
 
-    public static void makeSpicy(final PriorityQueue queue) {
+    private final void makeSpicy(final PriorityQueue queue) {
         int a = (int) queue.poll();
         int b = (int) queue.poll();
         int c = a + 2 * b;
