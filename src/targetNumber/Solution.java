0836e0e2bc9f9c3babdf3b164ac8816683409f94
leedongyeop <20181676@edu.hanbat.ac.kr>
2022-07-07 17:20:05 +0900

    깊이 우선 탐색 : 타겟 넘버 #43165 - 재귀 호출 방식으로 접근한 풀이

---
+++ src/targetNumber/Solution.java
@@ -0,0 +1,23 @@
+package targetNumber;
+
+public class Solution {
+    private int sum = 0;
+    private int index = 0;
+    private int count = 0;
+    public int solution(int[] numbers, int target) {
+
+        makeTarget(numbers, target, sum, index);
+
+        return count;
+    }
+
+    private final void makeTarget(final int[] numbers, final int target, final int sum, int index) {
+        if (index == numbers.length) {
+            if (sum == target) count++;
+        } else {
+            makeTarget(numbers, target, sum + numbers[index], index + 1);
+            makeTarget(numbers, target, sum - numbers[index], index + 1);
+//            makeTarget(numbers, target, sum - numbers[index], index++);
+        }
+    }
+}
